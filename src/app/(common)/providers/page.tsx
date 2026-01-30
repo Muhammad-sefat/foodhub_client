d8da80b1b7e96f2394f9e424431c68602bdceed7
Muhammad Sefat <muhammadsefat55@gmail.com>
2026-01-30 17:09:12 +0600

    complete the provider card and provider details page deign

--- src/app/(common)/providers/page.tsx
+++ src/app/(common)/providers/page.tsx
@@ -1,7 +1,35 @@
+import ProviderCard from "@/components/common/provider/ProviderCard";
+
+const providersResponse = {
+  success: true,
+  data: [
+    {
+      id: "bd385e93",
+      restaurant: "Burger House",
+      address: "Dhaka",
+    },
+    {
+      id: "03effdd3",
+      restaurant: "Pizza Palace",
+      address: "Dhaka",
+    },
+    {
+      id: "385f3bac",
+      restaurant: "Sandwich Corner",
+      address: "Baridhara Dhaka",
+    },
+  ],
+};
 export default function page() {
   return (
-    <div>
-      <h1>Providers</h1>
+    <div className="mx-auto max-w-7xl px-4 py-10">
+      <h1 className="mb-6 text-3xl font-bold text-black">Restaurants</h1>
+
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        {providersResponse.data.map((provider) => (
+          <ProviderCard key={provider.id} provider={provider} />
+        ))}
+      </div>
     </div>
   );
 }
